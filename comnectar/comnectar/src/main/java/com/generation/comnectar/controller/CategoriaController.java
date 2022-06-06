12846ed93bce78c5faa93bc1371b0510955951df
unknown <andreiflancanova@hotmail.com>
2022-06-06 12:31:56 -0300

    Upload Push, Put e Delete

--- comnectar/comnectar/src/main/java/com/generation/comnectar/controller/CategoriaController.java
+++ comnectar/comnectar/src/main/java/com/generation/comnectar/controller/CategoriaController.java
@@ -2,14 +2,22 @@ package com.generation.comnectar.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.generation.blogpessoal.model.Tema;
 import com.generation.comnectar.model.Categoria;
 import com.generation.comnectar.repository.CategoriaRepository;
 
@@ -46,4 +54,20 @@ public class CategoriaController {
 	public ResponseEntity<List<Categoria>> buscaCategoriaFrescor(@PathVariable boolean frescorCategoria) {
 		return ResponseEntity.ok(repository.findByFrescorCategoriaContainingIgnoreCase(frescorCategoria));
 	}
+	
+	@PostMapping
+	public ResponseEntity<Categoria> adicionaCategoria(@Valid @RequestBody Categoria categoria){
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(categoria));
+	}
+	
+	@PutMapping
+	public ResponseEntity<Categoria> atualizaCategoria(@Valid @RequestBody Categoria categoria){
+		return ResponseEntity.ok(repository.save(categoria));
+	}
+	
+	@DeleteMapping ("/{id}")
+	public void delete(@PathVariable Long id) {
+		repository.deleteById(id);
+	}
+	
 }
