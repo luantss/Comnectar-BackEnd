f979f347621e4e1bbef09ec80a12f5920392a1d7
unknown <lucianarocha_14@hotmail.com>
2022-06-07 10:24:36 -0300

    Criacao da model usuario

---
+++ comnectar/comnectar/src/main/java/com/generation/comnectar/model/Usuario.java
@@ -0,0 +1,83 @@
+package com.generation.comnectar.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name="tb_usuario")
+public class Usuario {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotNull
+	private String nomeUsuario;
+	
+	@NotNull
+	@Email
+	private String loginUsuario;
+	
+	@NotNull
+	@Size(min = 8, max = 20)
+	private String senhaUsuario;
+	
+	@NotNull
+	private String localUsuario;
+	
+	private String fotoUsuario;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNomeUsuario() {
+		return nomeUsuario;
+	}
+
+	public void setNomeUsuario(String nomeUsuario) {
+		this.nomeUsuario = nomeUsuario;
+	}
+
+	public String getLoginUsuario() {
+		return loginUsuario;
+	}
+
+	public void setLoginUsuario(String loginUsuario) {
+		this.loginUsuario = loginUsuario;
+	}
+
+	public String getSenhaUsuario() {
+		return senhaUsuario;
+	}
+
+	public void setSenhaUsuario(String senhaUsuario) {
+		this.senhaUsuario = senhaUsuario;
+	}
+
+	public String getLocalUsuario() {
+		return localUsuario;
+	}
+
+	public void setLocalUsuario(String localUsuario) {
+		this.localUsuario = localUsuario;
+	}
+
+	public String getFotoUsuario() {
+		return fotoUsuario;
+	}
+
+	public void setFotoUsuario(String fotoUsuario) {
+		this.fotoUsuario = fotoUsuario;
+	}
+	
+}
