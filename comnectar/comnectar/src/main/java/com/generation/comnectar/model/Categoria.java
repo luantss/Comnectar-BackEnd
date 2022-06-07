da0311aba8b74497da7768b863e96935379e7d12
unknown <andreiflancanova@hotmail.com>
2022-06-07 14:59:04 -0300

    Upload Correções e Testes 07/06 - Parte 01

--- comnectar/comnectar/src/main/java/com/generation/comnectar/model/Categoria.java
+++ comnectar/comnectar/src/main/java/com/generation/comnectar/model/Categoria.java
@@ -1,11 +1,15 @@
 package com.generation.comnectar.model;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name="tb_categoria")
@@ -26,6 +30,9 @@ public class Categoria {
 	
 	@NotNull
 	private boolean frescorCategoria;
+	
+	@OneToMany(mappedBy = "categoria", cascade = CascadeType.ALL)
+	@JsonIgnoreProperties("categoria")
 
 	public Long getId() {
 		return id;
