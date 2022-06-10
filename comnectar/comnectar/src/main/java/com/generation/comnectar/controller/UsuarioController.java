5eba21ad6cc049368f97544666193347b8700634
unknown <andreiflancanova@hotmail.com>
2022-06-10 12:24:14 -0300

    Upload UsuarioController + UsuarioService

---
+++ comnectar/comnectar/src/main/java/com/generation/comnectar/controller/UsuarioController.java
@@ -0,0 +1,39 @@
+package com.generation.comnectar.controller;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.generation.comnectar.model.Usuario;
+import com.generation.comnectar.model.UsuarioLogin;
+import com.generation.comnectar.service.UsuarioService;
+
+@RestController
+@RequestMapping("/usuarios")
+@CrossOrigin(origins ="*",allowedHeaders = "*")
+public class UsuarioController {
+
+	@Autowired
+	private UsuarioService usuarioService;
+
+	@PostMapping("/cadastrar")
+	public ResponseEntity<Optional<Usuario>> Post(@RequestBody Usuario usuario){
+		return ResponseEntity.status(HttpStatus.CREATED)
+				.body(usuarioService.cadastrarUsuario(usuario));
+	}
+
+	@PostMapping("/logar")
+	public ResponseEntity<UsuarioLogin> Authentication(@RequestBody Optional<UsuarioLogin> user){
+		return usuarioService.autenticarUsuario(user).map(resposta -> ResponseEntity.ok(resposta))
+				.orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
+	}
+
+
+}
---
+++ comnectar/comnectar/src/main/java/com/generation/comnectar/service/UsuarioService.java
@@ -0,0 +1,99 @@
+package com.generation.comnectar.service;
+
+import java.nio.charset.Charset;
+import java.util.Optional;
+
+import org.apache.commons.codec.binary.Base64;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.generation.comnectar.model.Usuario;
+import com.generation.comnectar.model.UsuarioLogin;
+import com.generation.comnectar.repository.UsuarioRepository;
+
+@Service
+public class UsuarioService {
+
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+
+	public Optional<Usuario> cadastrarUsuario(Usuario usuario) {
+
+		if (usuarioRepository.findByLoginUsuario(usuario.getLoginUsuario()).isPresent())
+			return Optional.empty();
+
+		usuario.setSenhaUsuario(criptografarSenha(usuario.getSenhaUsuario()));
+
+		return Optional.of(usuarioRepository.save(usuario));
+
+	}
+
+	public Optional<Usuario> atualizarUsuario(Usuario usuario) {
+
+		if (usuarioRepository.findById(usuario.getId()).isPresent()) {
+
+			Optional<Usuario> buscaLoginUsuario = usuarioRepository.findByLoginUsuario(usuario.getLoginUsuario());
+
+			if (buscaLoginUsuario.isPresent()) {				
+				if (buscaLoginUsuario.get().getId() != usuario.getId())
+					throw new ResponseStatusException(
+							HttpStatus.BAD_REQUEST, "O Usuário já existe!", null);
+			}
+
+			usuario.setSenhaUsuario(criptografarSenha(usuario.getSenhaUsuario()));
+
+			return Optional.of(usuarioRepository.save(usuario));
+		} 
+
+		return Optional.empty();
+	}
+
+	public Optional<UsuarioLogin> autenticarUsuario(Optional<UsuarioLogin> usuarioLogin) {
+
+		Optional<Usuario> usuario = usuarioRepository.findByLoginUsuario(usuarioLogin.get().getUsuario());
+
+		if (usuario.isPresent()) {
+			if (compararSenhas(usuarioLogin.get().getSenha(), usuario.get().getSenhaUsuario())) {
+
+				usuarioLogin.get().setNome(usuario.get().getNomeUsuario());
+				usuarioLogin.get().setSenha(usuario.get().getSenhaUsuario());
+				usuarioLogin.get().setToken(gerarBasicToken(usuarioLogin.get().getUsuario(), usuarioLogin.get().getSenha()));
+
+				return usuarioLogin;
+
+			}
+		}	
+
+		return Optional.empty();
+
+	}
+
+	private String criptografarSenha(String senha) {
+
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+		return encoder.encode(senha);
+
+	}
+
+	private boolean compararSenhas(String senhaDigitada, String senhaBD) {
+
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+		return encoder.matches(senhaDigitada, senhaBD);
+
+	}
+
+	private String gerarBasicToken(String email, String password) {
+
+		String tokenBase = email + ":" + password;
+		byte[] tokenBase64 = Base64.encodeBase64(tokenBase.getBytes(Charset.forName("US-ASCII")));
+		return "Basic " + new String(tokenBase64);
+
+	}
+
+
+}
