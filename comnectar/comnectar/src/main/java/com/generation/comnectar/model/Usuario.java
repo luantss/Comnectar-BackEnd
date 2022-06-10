428dd1d6e4bdf8201f54e727c2c9b0c88ae51481
Tacio <taciosfer@gmail.com>
2022-06-10 10:17:55 -0300

    Alterações na model Usuario+UsuarioLogin e criação UsuarioRepository

--- comnectar/comnectar/src/main/java/com/generation/comnectar/model/Usuario.java
+++ comnectar/comnectar/src/main/java/com/generation/comnectar/model/Usuario.java
@@ -1,7 +1,6 @@
 package com.generation.comnectar.model;
 
 import java.util.List;
-
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -12,7 +11,6 @@ import javax.persistence.Table;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
