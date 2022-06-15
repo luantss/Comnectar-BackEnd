430324173a067932f2d7dcf0d0ccf8e864e715f1
unknown <luan.teixeira.saramago@gmail.com>
2022-06-15 11:07:29 -0300

    Add: get usuario id e atualizar usuario

--- comnectar/comnectar/src/main/java/com/generation/comnectar/controller/UsuarioController.java
+++ comnectar/comnectar/src/main/java/com/generation/comnectar/controller/UsuarioController.java
@@ -3,12 +3,16 @@ package com.generation.comnectar.controller;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,6 +37,13 @@ public class UsuarioController {
 	public ResponseEntity<List<Usuario>> buscaUsuarios(){
 		return ResponseEntity.ok(usuarioRepository.findAll());
 	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Usuario> getById(@PathVariable Long id) {
+		return usuarioRepository.findById(id)
+				.map(resposta -> ResponseEntity.ok(resposta))
+				.orElse(ResponseEntity.notFound().build());
+	}
 
 	@PostMapping("/cadastrar")
 	public ResponseEntity<Optional<Usuario>> Post(@RequestBody Usuario usuario){
@@ -45,6 +56,12 @@ public class UsuarioController {
 		return usuarioService.autenticarUsuario(user).map(resposta -> ResponseEntity.ok(resposta))
 				.orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
 	}
-
+	
+	@PutMapping("/atualizar")
+	public ResponseEntity<Usuario> putUsuario(@Valid @RequestBody Usuario usuario) {
+		return usuarioService.atualizarUsuario(usuario)
+				.map(resposta -> ResponseEntity.status(HttpStatus.OK).body(resposta))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
 
 }
